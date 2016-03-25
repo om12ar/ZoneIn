b3fc62ca5a0e386c3eb0db8bb88cc09fe8c8e8fd
NohaDiaa <noha.ma7moud@live.com>
2016-03-26 00:45:49 +0200

    xml files UserProfile class and fragments

---
+++ app/src/main/java/com/swe/zonein/zonein/Activities/TopUserProfileFragment.java
@@ -0,0 +1,23 @@
+package com.swe.zonein.zonein.Activities;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.swe.zonein.zonein.R;
+
+/**
+ * Created by Noha on 3/26/2016.
+ */
+public class TopUserProfileFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.top_user_profile_fragment, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/swe/zonein/zonein/Activities/UserProfileActivity.java
@@ -0,0 +1,8 @@
+package com.swe.zonein.zonein.Activities;
+
+/**
+ * Created by Noha on 3/26/2016.
+ */
+public class UserProfileActivity {
+    //check lw el user da hwa nfs el user wla l2 , lw l2 bn5li el follow button invisible (m3rfsh ezay)
+}
