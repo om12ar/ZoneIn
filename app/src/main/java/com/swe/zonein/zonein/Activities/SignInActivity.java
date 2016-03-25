0f0e9ea3b855d599c688c1afbe125eb74d39d521
om12ar <ois2@ymail.com>
2016-03-25 04:42:01 +0200

    testing JSON URLs

--- app/src/main/java/com/swe/zonein/zonein/Activities/SignInActivity.java
+++ app/src/main/java/com/swe/zonein/zonein/Activities/SignInActivity.java
@@ -1,6 +1,7 @@
 package com.swe.zonein.zonein.Activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +15,13 @@ import com.swe.zonein.zonein.Controllers.DBController;
 import com.swe.zonein.zonein.Controllers.MainController;
 import com.swe.zonein.zonein.Models.User;
 import com.swe.zonein.zonein.R;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -34,16 +42,19 @@ public class SignInActivity extends AppCompatActivity {
                 final EditText passET = (EditText) findViewById(R.id.passET);
                 final TextView statusET = (TextView) findViewById(R.id.statusET);
                 Log.e(TAG, "INLOGIN");
+                loginTask task = new loginTask();
+                String s = task.execute("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a").toString();
+                Log.e(TAG, s);
                 int id = db.login(nameET.getText().toString(), passET.getText().toString());
                 if (id != -1) {
-                    Log.e(TAG, "INLOGIN");
-                    statusET.setText("SUCCESS");
+                    Log.e(TAG, s);
+                    statusET.setText(s);
                     MainController.user = new User(db.getUser(id));
                     Intent intent = new Intent(SignInActivity.this, MainActivity.class);
                     System.err.print("GOT USER FROM DB");
                     Log.e("LOGIN ACTIVITY", MainController.user.getName());
-                    startActivity(intent);
-                    finish();
+                    //startActivity(intent);
+                    //finish();
                 } else {
                     statusET.setText("Wrong Email or Password ");
                 }
@@ -71,6 +82,39 @@ public class SignInActivity extends AppCompatActivity {
         });
 
 
+    }
+    public class loginTask extends AsyncTask<String ,Void , String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String result = "";
+            HttpURLConnection urlConnection = null ;
+            URL url ;
+            try {
+                url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                //urlConnection = url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setDoOutput(true);
+                urlConnection.connect();
+                InputStream inputStream = urlConnection.getInputStream() ;
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+                while(data!=-1){
+                    char current = (char) data;
+                    result += current ;
+                    data = inputStreamReader.read();
+                }
+
+                Log.e(TAG, result);
+                return result;
+
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 
 }
---
+++ app/src/main/java/com/swe/zonein/zonein/Controllers/JSONParser.java
@@ -0,0 +1,7 @@
+package com.swe.zonein.zonein.Controllers;
+
+/**
+ * Created by om12ar on 3/25/16.
+ */
+public class JSONParser {
+}
